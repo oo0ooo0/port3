091967bdf37be4b5430601b87de343e696fbeaa8
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-05 21:23:52 +0900

    index.js + sec1:Title
    index.js + sec1:Title

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './reset.css';
+import Sec1_title from './Sec1_title';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Sec1_title/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
