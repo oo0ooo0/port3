a754fe96532ad480139ed2305dc67593b9f650e9
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-09 12:50:31 +0900

    sec1:Title_ani
    for sec1:Title animation
    - add js at index.html
    - add jq

--- src/Sec1_title.js
+++ src/Sec1_title.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useEffect, useCallback, useRef } from 'react';
 import './Sec1_title.css';
 
 function Sec1_title() {
+    const ref = useRef(null);
+    const start = useCallback(() => {
+        if(ref.current) {
+            
+            ref.current.classList.add('show');
+        }        
+       }
+    , []);
+    useEffect(() => {
+        setTimeout(start, 1000);
+  
+    }, []);
   return (
-    // in js remove class show
-    <section id="section1" class ="show">
-    <h2 class="ir_su">section1_title</h2>
-    <div class="container">
-        <div class="row">
-            <div class="sec1">
-                <div class="text-wrap">
-                    <div class="top-menu">
+    // in js remove className show
+    <section id="section1" ref={ref}>
+    <h2 className="ir_su">section1_title</h2>
+    <div className="container">
+        <div className="row">
+            <div className="sec1">
+                <div className="text-wrap">
+                    <div className="top-menu">
                         <ul>
                             <li><a href="https://oo0ooo0.tistory.com/" target="_blank">Blog</a></li>
                             <li><a href="mailto:kkjjajja@gmail.com" target="_blank">Email</a></li>
                         </ul>
                     </div>
-                    <h1 class="line">
-                        <svg class="svgWrap" viewBox="0 0 1000 550" xmlns='http://www.w3.org/2000/svg' version='1.1'>
-                            <text class="aniText" font-size='210px' x="50%" y="50%" dy=".35em"
-                                text-anchor="middle">portfolio</text>
+                    <h1 className="line">
+                        <svg className="svgWrap" viewBox="0 0 1000 550" xmlns='http://www.w3.org/2000/svg' version='1.1'>
+                            <text className="aniText" fontSize='210px' x="50%" y="50%" dy=".35em"
+                                textAnchor="middle">portfolio</text>
                         </svg>
-                        <span class="mail">KWON JAMIN || <a href="mailto:kkjjajja@gmail.com"
+                        <span className="mail">KWON JAMIN || <a href="mailto:kkjjajja@gmail.com"
                                 target="blank">kkjjajja@gmail.com</a></span>
                     </h1>
                 </div>
