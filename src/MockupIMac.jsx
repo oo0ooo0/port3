efe1088b8e52765cbd5cbe90232035e49f41aa3a
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-10 17:57:26 +0900

    section5_portfolioList
    section5_portfolioList

---
+++ src/MockupIMac.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useCallback, useRef, useState } from 'react';
+import './MockupIMac.css';
+
+function MockupIMac() {
+  return (
+    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0vw" y="0vw" width="100%" viewBox="0 0 719.91 579.928" enableBackground="new 0 0 719.91 579.928">
+        <polyline fill="#D1000A" points="289,497.5 268.5,569 242,575.5 242,579.928 480,579.928 480,574.5 455.5,569 433.5,497.5 " />
+        <path fill="#D1000A" d="M702.91,0H17C7.611,0,0,7.611,0,17v461c0,9.389,7.611,17,17,17h685.91c9.389,0,17-7.611,17-17V17C719.91,7.611,712.299,0,702.91,0z M362.5,478.625c-9.941,0-18-8.059-18-18s8.059-18,18-18s18,8.059,18,18S372.441,478.625,362.5,478.625z" />
+    </svg>
+  );
+}
+
+export default MockupIMac;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Sec1_title from './Sec1_title';
 import Sec2_iphone from './Sec2_iphone';
 import Sec3_blog from './Sec3_blog';
 import Sec4_textAnimation from './Sec4_textAnimation';
+import Sec5_portfolioList from './Sec5_portfolioList';
 import * as serviceWorker from './serviceWorker';
 
 
@@ -13,6 +14,8 @@ ReactDOM.render(<Sec1_title/>, document.getElementById('root'));
 ReactDOM.render(<Sec2_iphone/>, document.getElementById('root2'));
 ReactDOM.render(<Sec3_blog/>, document.getElementById('root3'));
 ReactDOM.render(<Sec4_textAnimation/>, document.getElementById('root4'));
+ReactDOM.render(<Sec5_portfolioList/>, document.getElementById('root5'));
+
 
 
 serviceWorker.unregister();
