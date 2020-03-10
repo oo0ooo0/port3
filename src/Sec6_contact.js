ac0a8447c4268dcf369496376280e9233c19ede2
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-10 18:54:22 +0900

    section6_contact
    section6_contact

---
+++ src/Sec6_contact.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useCallback, useRef } from 'react';
+import './Sec6_contact.css';
+
+function Sec6_contact() {
+   
+  return (
+    <section id="section6">
+    <h2 class="ir_su"> section6_contact</h2>
+    <div class="container">
+        <div class="row">
+            <div class="sec6">
+                <div class="line">
+                    <div class="miniProfile">
+                        <p class="name">
+                            KWON JAMIN
+                        </p>
+                         <p class="email">
+                            kkjajjja@gmail.com
+                        </p>
+                     </div>
+                    <div class="footer">
+                        <p>Copyrightⓒ2020.권자민.All rights reserved.</p>
+                        <p> This site is intended as a personal portfolio site, not for commercial use.  </p>                    
+                        </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
+   );
+}
+
+export default Sec6_contact;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Sec2_iphone from './Sec2_iphone';
 import Sec3_blog from './Sec3_blog';
 import Sec4_textAnimation from './Sec4_textAnimation';
 import Sec5_portfolioList from './Sec5_portfolioList';
+import Sec6_contact from './Sec6_contact';
 import * as serviceWorker from './serviceWorker';
 
 
@@ -15,6 +16,7 @@ ReactDOM.render(<Sec2_iphone/>, document.getElementById('root2'));
 ReactDOM.render(<Sec3_blog/>, document.getElementById('root3'));
 ReactDOM.render(<Sec4_textAnimation/>, document.getElementById('root4'));
 ReactDOM.render(<Sec5_portfolioList/>, document.getElementById('root5'));
+ReactDOM.render(<Sec6_contact/>, document.getElementById('root6'));
 
 
 
