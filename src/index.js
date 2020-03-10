bf1581eb0d0e8da83a51ea800354f61c4b38c830
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-10 17:40:38 +0900

    section4_textAnimation
    section4_textAnimation

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import './reset.css';
 import Sec1_title from './Sec1_title';
 import Sec2_iphone from './Sec2_iphone';
 import Sec3_blog from './Sec3_blog';
+import Sec4_textAnimation from './Sec4_textAnimation';
 import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(<Sec1_title/>, document.getElementById('root'));
 ReactDOM.render(<Sec2_iphone/>, document.getElementById('root2'));
 ReactDOM.render(<Sec3_blog/>, document.getElementById('root3'));
+ReactDOM.render(<Sec4_textAnimation/>, document.getElementById('root4'));
+
 
 serviceWorker.unregister();
