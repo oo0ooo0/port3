f7d1853ee24e0274e1e1515c70dc939a74754e17
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-10 17:28:43 +0900

    section3_blog
    add section3_blog

--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import './reset.css';
 import Sec1_title from './Sec1_title';
 import Sec2_iphone from './Sec2_iphone';
+import Sec3_blog from './Sec3_blog';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<Sec1_title/>, document.getElementById('root'));
 ReactDOM.render(<Sec2_iphone/>, document.getElementById('root2'));
+ReactDOM.render(<Sec3_blog/>, document.getElementById('root3'));
 
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
