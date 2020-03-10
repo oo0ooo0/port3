7982232bb6f754eb4796551cf977a941b0088754
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-03-10 14:21:49 +0900

    add Sec2
    Sec2: IPhone Animation

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './reset.css';
 import Sec1_title from './Sec1_title';
+import Sec2_iphone from './Sec2_iphone';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Sec1_title/>, document.getElementById('root'));
+ReactDOM.render(<Sec2_iphone/>, document.getElementById('root2'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
